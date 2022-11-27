1b3c77440d55f77f496fbc77ddc337d679585920
Alsu <shakilsu@yandex.ru>
2022-11-27 16:54:13 +0300

    task34

---
+++ Library.cs
@@ -0,0 +1,101 @@
+public static class Library
+{
+    // обязательно в новом файле прописываем 
+    // using static Library
+
+
+    // метод, который создаёт массив
+    public static int[] CreateArray(int count)
+    {
+     return new int[count];
+    }
+    // int[] array = CreateArray(10);
+
+    // заполняет массив
+    public static void Fill(int[] array, int min, int max)
+    { 
+        int size = array.Length; 
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = Random.Shared.Next(min, max + 1);
+            //array[i] = new Random().Next(min, max);
+        }
+    }
+    // Fill(array, -5, 5);
+
+    // печать массива
+    public static string PrintGood(int[] numbers)
+    {
+        int size = numbers.Length;
+        int i = 0;
+        string result = "[ ";
+
+        while (i < size)
+        {
+            result += ($"{numbers[i],3} ");
+            i++;
+        }
+        return result + "]";
+    }
+    // Console.WriteLine(PrintGood(array));
+
+    // вычисление суммы положительных значений
+    public static int SumPositive(int[] numbers)
+    {
+        int result = 0;
+        int size = numbers.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (numbers[i] > 0) result += numbers[i];
+        }
+        return result;
+    }
+    // int pos = SumPositive(col);
+    // Console.WriteLine($">0 {pos}");
+
+    // вычисление суммы отрицательных значений
+    public static int SumNegative(int[] numbers)
+    {
+        int result = 0;
+        int size = numbers.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (numbers[i] < 0) result += numbers[i];
+        }
+        return result;
+    }
+    // int neg = SumNegative(col);
+    // Console.WriteLine($"<0 {neg}");
+
+    // метод упорядочивания элементов
+    public static void SelectionSort(int[] array)
+    {
+        for(int i = 0; i < array.Length - 1; i++)
+        {
+            int minPosition = i;
+            for(int j = i + 1; j < array.Length; j++)
+            {
+                if(array[j] < array[minPosition]) minPosition = j;
+            }
+            int temporary = array[i];
+            array[i] = array[minPosition];
+            array[minPosition] = temporary;
+        }
+    }
+    //SelectionSort(array);
+
+    //подсчет четных чисел
+    public static int Multiple(int[] array)
+    {
+        int result = 0;
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if((array[i] % 2) == 0) result++; ;
+        }
+        return result;
+    }
+    //int result = Multiple(array);
+    //Console.WriteLine($"-> {result}");
+
+}
--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,49 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
+
+
+int[] CreateArray(int count)
+    {
+     return new int[count];
+    }
+
+void Fill(int[] array, int min, int max)
+{ 
+    int size = array.Length; 
+    for (int i = 0; i < size; i++)
+    {
+        //array[i] = Random.Shared.Next(min, max + 1);
+        array[i] = new Random().Next(min, max+ 1);
+    }
+}
+
+string PrintGood(int[] numbers)
+{
+    int size = numbers.Length;
+    int i = 0;
+    string result = "[ ";
+
+    while (i < size)
+    {
+        result += ($"{numbers[i],3} ");
+        i++;
+    }
+    return result + "]";
+}
+
+int Multiple(int[] array)
+{
+    int result = 0;
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if((array[i] % 2) == 0) result++; ;
+    }
+    return result;
+}
+
+int[] array = CreateArray(10);
+Fill(array, 100, 999);
+int result = Multiple(array);
+Console.WriteLine($"{PrintGood(array)} -> {result}");
